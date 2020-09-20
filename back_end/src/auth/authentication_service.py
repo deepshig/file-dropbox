6d44ab65b3daa8bf33981fe9819b91dc4a088369
deepshig <deepshi1104@gmail.com>
2020-09-20 14:16:52 +0200

    Add service layer logic to fetch user details

--- back_end/src/auth/authentication_service.py
+++ back_end/src/auth/authentication_service.py
@@ -1,6 +1,8 @@
 import uuid
 from . import user_db
 
+ERROR_UNAUTHORISED_REQUEST = "User not authorised to fetch this resource"
+
 
 class Authenticator:
     def __init__(self, db_config):
@@ -17,3 +19,14 @@ class Authenticator:
             return user_details
         else:
             return result
+
+    def get_user(self, user_id, access_token):
+        fetched_user = self.db.get_user(user_id)
+        if fetched_user["user_fetched"]:
+            if fetched_user["access_token"] == access_token:
+                return fetched_user
+            else:
+                return {"user_fetched": False,
+                        "error": ERROR_UNAUTHORISED_REQUEST}
+        else:
+            return fetched_user
--- back_end/tests/auth/test_authentication_service.py
+++ back_end/tests/auth/test_authentication_service.py
@@ -5,8 +5,8 @@ import psycopg2
 from psycopg2 import Error
 sys.path.append('../')
 
-from src.auth.user_db import UserDB, ERROR_USER_NOT_FOUND  # NOQA
-from src.auth.authentication_service import Authenticator  # NOQA
+from src.auth.user_db import ERROR_USER_NOT_FOUND  # NOQA
+from src.auth.authentication_service import Authenticator, ERROR_UNAUTHORISED_REQUEST  # NOQA
 
 test_db_config = {"user": "postgres",
                   "password": "postgres",
@@ -41,3 +41,47 @@ def test_create_user():
         print("Error while fetching the test user : ", err)
     finally:
         tear_down(cursor, auth.db.db_driver)
+
+
+def test_get_user():
+    """
+    failure : user does not exist
+    """
+    auth = Authenticator(test_db_config)
+    user_id = uuid.uuid4()
+    access_token = uuid.uuid4()
+
+    fetched_user = auth.get_user(user_id, access_token)
+    assert fetched_user["user_fetched"] == False
+    assert fetched_user["error"] == ERROR_USER_NOT_FOUND
+
+    """
+    failure : user not authorrised to view this user
+    """
+    correct_access_token = uuid.uuid4()
+    create_user_query = '''INSERT INTO users(id, role, access_token, logged_in, created_at, updated_at) VALUES ((%s), (%s), (%s), (%s), now(), now())'''
+    cursor = auth.db.db_driver.connection.cursor()
+    psycopg2.extras.register_uuid()
+
+    try:
+        cursor.execute(create_user_query, [
+            user_id, "admin", correct_access_token, False])
+        auth.db.db_driver.connection.commit()
+    except psycopg2.Error as err:
+        print("Error in creating test user : ", err)
+
+    fetched_user = auth.get_user(user_id, access_token)
+    assert fetched_user["user_fetched"] == False
+    assert fetched_user["error"] == ERROR_UNAUTHORISED_REQUEST
+
+    """
+    success
+    """
+    fetched_user = auth.get_user(user_id, correct_access_token)
+    assert fetched_user["user_fetched"] == True
+    assert fetched_user["id"] == user_id
+    assert fetched_user["access_token"] == correct_access_token
+    assert fetched_user["role"] == "admin"
+    assert fetched_user["logged_in"] == False
+
+    tear_down(cursor, auth.db.db_driver)
