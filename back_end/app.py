5df4f5615abeb08b16c156ac3bd06711bb525bf2
Diarmuid Kelly <diarmuid100@gmail.com>
2020-09-25 15:19:56 +0200

    Cleaning

--- back_end/app.py
+++ back_end/app.py
@@ -1,19 +1,3 @@
-from flask import Flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from flask_cors import CORS
-from back_end.API import socket, API
-from back_end.src.auth import authenticator
-app = Flask(__name__)
+from back_end import app
 
-CORS(app, supports_credentials=True)
-
-app.config['SECRET_KEY'] = 'super-secret-key'
-app.config['JWT_SECRET_KEY'] = 'super-secret-key' # TODO: Can this just be SECRET_KEY?
-application = DispatcherMiddleware(API, {
-    '/auth': authenticator,
-    '/socket': socket
-})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True, port=4000  )
+app.run()
