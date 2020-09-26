38640fe533b4ee3a2ba7c33b7bfe6e859125d31e
deepshig <deepshi1104@gmail.com>
2020-09-26 20:14:23 +0200

    Add APIs for create user, login and logout

--- back_end/src/auth/app.py
+++ back_end/src/auth/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, make_response, jsonify
 from flask_restful import Resource, Api, output_json
+from uuid import UUID
 from . import authentication_service, user_db
 # import authentication_service
 # import user_db
@@ -8,6 +9,7 @@ app = Flask(__name__)
 api = Api(app)
 
 ERROR_ROLE_NOT_FOUND = "Invalid role"
+ERROR_INVALID_USER_ID = "Inavlid User ID"
 ERROR_INTERNAL_SERVER = "Internal Server Error"
 
 db_config = {"user": "postgres",
@@ -25,12 +27,81 @@ def init(db_config):
     return svc
 
 
+def is_valid_uuid(uuid_str):
+    try:
+        uuid_obj = UUID(uuid_str, version=4)
+    except ValueError:
+        return False
+
+    return str(uuid_obj) == uuid_str
+
+
 class Ping(Resource):
     def get(self):
         return output_json({"ping": "pong"}, 200)
 
 
+class CreateUser(Resource):
+    def __init__(self, svc):
+        self.svc = svc
+
+    def post(self, role):
+        if role not in accepted_roles:
+            return output_json({"msg": ERROR_ROLE_NOT_FOUND}, 400)
+
+        user_details = svc.create_user(role)
+        if user_details["user_created"]:
+            user_details["id"] = str(user_details["id"])
+            user_details["access_token"] = str(user_details["access_token"])
+            return output_json(user_details, 201)
+        else:
+            return output_json({"msg": ERROR_INTERNAL_SERVER}, 500)
+
+
+class LoginUser(Resource):
+    def __init__(self, svc):
+        self.svc = svc
+
+    def put(self, user_id):
+        if not is_valid_uuid(user_id):
+            return output_json({"msg": ERROR_INVALID_USER_ID}, 400)
+
+        response = svc.login(user_id)
+        if response["user_logged_in"]:
+            response["access_token"] = str(response["access_token"])
+            return output_json(response, 201)
+        elif response["error"] == user_db.ERROR_USER_NOT_FOUND:
+            return output_json(response, 400)
+        else:
+            return output_json({"msg": ERROR_INTERNAL_SERVER}, 500)
+
+
+class LogoutUser(Resource):
+    def __init__(self, svc):
+        self.svc = svc
+
+    def put(self, user_id):
+        if not is_valid_uuid(user_id):
+            return output_json({"msg": ERROR_INVALID_USER_ID}, 400)
+
+        response = svc.logout(user_id)
+        if response["user_logged_out"]:
+            return output_json(response, 200)
+        elif response["error"] == user_db.ERROR_USER_NOT_FOUND:
+            return output_json(response, 400)
+        else:
+            return output_json({"msg": ERROR_INTERNAL_SERVER}, 500)
+
+
+svc = init(db_config)
+
 api.add_resource(Ping, '/ping')
+api.add_resource(CreateUser, '/auth/signup/<string:role>',
+                 resource_class_kwargs={"svc": svc})
+api.add_resource(LoginUser, '/auth/login/<string:user_id>',
+                 resource_class_kwargs={"svc": svc})
+api.add_resource(LogoutUser, '/auth/logout/<string:user_id>',
+                 resource_class_kwargs={"svc": svc})
 
 if __name__ == '__main__':
     app.run(debug=True, use_debugger=False, use_reloader=False,
