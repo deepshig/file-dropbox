7e0b702f91854b1fcf0dea59990eb1cedff152ea
deepshig <deepshi1104@gmail.com>
2020-09-30 14:05:50 +0200

    Internalise the logic to create a new redis connection

--- back_end/src/file_uploader/redis.py
+++ back_end/src/file_uploader/redis.py
@@ -1,15 +1,16 @@
 import redis
+from redis import RedisError
 import sys
 
 
 class RedisDriver:
-    def __init__(self):
-        self.connection = None
+    def __init__(self, redis_config):
+        self.__connect(redis_config)
 
-    def connect(self, redis_config):
+    def __connect(self, redis_config):
         try:
             self.connection = redis.Redis(host=redis_config["host"],
                                           port=redis_config["port"])
-        except Exception as err:
+        except RedisError as err:
             error_str = "Error while connecting to redis : " + str(err)
             sys.exit(error_str)
