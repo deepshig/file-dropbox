d7270e48a09563cc8ace60eb513ea5b216ba9726
deepshig <deepshi1104@gmail.com>
2020-09-30 14:11:44 +0200

    Add logic to set value in redis

--- back_end/src/file_uploader/redis.py
+++ back_end/src/file_uploader/redis.py
@@ -14,3 +14,14 @@ class RedisDriver:
         except RedisError as err:
             error_str = "Error while connecting to redis : " + str(err)
             sys.exit(error_str)
+
+    def set(self, key, value):
+        key_str = str(key)
+        val_str = str(value)
+        try:
+            self.connection.set(key_str, val_str)
+            return {"success": True}
+        except RedisError as err:
+            error_str = "Error while connecting to redis : " + str(err)
+            return {"success": False,
+                    "error": error_str}
--- back_end/tests/file_uploader/test_redis.py
+++ back_end/tests/file_uploader/test_redis.py
@@ -14,8 +14,26 @@ def teardown_redis(redis_conn):
 
 
 def test_connect():
-    r = RedisDriver()
-    r.connect(test_redis_config)
+    r = RedisDriver(test_redis_config)
     r.connection.set("hello", "world")
     assert r.connection.get("hello") == b'world'
+    teardown_redis(r.connection)
+
+
+def test_set():
+    r = RedisDriver(test_redis_config)
+    """
+    set normal string value
+    """
+    result = r.set("test_key", "test_value")
+    assert result["success"] == True
+
+    assert r.connection.get("test_key") == b'test_value'
+
+    """
+    set non-string value
+    """
+    result = r.set("test_key2", {"k1": 1})
+    print(result)
+    assert result["success"] == True
     teardown_redis(r.connection)
