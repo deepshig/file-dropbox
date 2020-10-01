cf3ca7f6b2bfa9663554282e0fe3381574450270
deepshig <deepshi1104@gmail.com>
2020-10-01 14:30:15 +0200

    Add logic to publish an event in the RabbitMQ queue

--- back_end/src/file_uploader/rabbitmq.py
+++ back_end/src/file_uploader/rabbitmq.py
@@ -3,31 +3,53 @@ import sys
 
 
 class RabbitMQManager:
-    def __init__(self, amqp_url, queue_name):
-        self.connection = self.__get_connection(amqp_url)
-        self.queue_name = queue_name
+    def __init__(self, rabbitmq_config):
+        self.connection_url = self.__get_connection_url(rabbitmq_config)
+        self.connection = self.__get_connection(self.connection_url)
+        self.queue_name = rabbitmq_config["queue_name"]
         self.__init_queue()
+
+    def __get_connection_url(self, rabbitmq_config):
+        return "amqp://{}:{}@{}:{}?connection_attempts=10&retry_delay=10".format(rabbitmq_config["user"],
+                                                                                 rabbitmq_config["password"],
+                                                                                 rabbitmq_config["host"],
+                                                                                 rabbitmq_config["port"])
 
     def __get_connection(self, amqp_url):
         try:
             url_params = pika.URLParameters(amqp_url)
             connection = pika.BlockingConnection(url_params)
-            return connection
         except pika.exceptions as err:
             error_str = "Error while connecting to rabbitmq : " + str(err)
             sys.exit(error_str)
+        else:
+            return connection
 
     def __init_queue(self):
         try:
             chan = self.connection.channel()
             chan.queue_declare(queue=self.queue_name, durable=True)
-            return
         except pika.exceptions as err:
             error_str = "Error while creating queue : " + str(err)
             sys.exit(error_str)
+        else:
+            return
 
     def shutdown_queue(self):
         chan = self.connection.channel()
         chan.close()
         self.connection.close()
         return
+
+    def publish(self, message_body):
+        try:
+            chan = self.connection.channel()
+            chan.basic_publish(exchange='', routing_key=self.queue_name,
+                               body=message_body, properties=pika.BasicProperties(delivery_mode=2))
+        except pika.exceptions as err:
+            err_str = "Error while publishing message to queue=" + \
+                self.queue_name + " : " + str(err)
+            return {"message_published": False,
+                    "error": err_str}
+        else:
+            return {"message_published": True}
---
+++ back_end/tests/file_uploader/test_rabbitmq.py
@@ -0,0 +1,52 @@
+import pytest
+import sys
+import pika
+sys.path.append('../')
+
+from src.file_uploader.rabbitmq import RabbitMQManager  # NOQA
+
+test_rabbitmq_config = {"user": "guest",
+                        "password": "guest",
+                        "host": "127.0.0.1",
+                        "port": "5672",
+                        "queue_name": "test"}
+
+
+def test_publish():
+    """
+    success
+    """
+    queue = RabbitMQManager(test_rabbitmq_config)
+    message = "Hello World"
+    result = queue.publish(message)
+    assert result["message_published"] == True
+
+    msg_received = []
+    start_test_consumer(msg_received)
+    assert msg_received[0] == message
+    queue.shutdown_queue()
+
+
+def receive_msg_wrapper(msg_received, chan):
+    def receive_msg(ch, method, properties, body):
+        msg_received.append(body.decode('utf-8'))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        chan.stop_consuming()
+    return receive_msg
+
+
+def start_test_consumer(msg_received):
+    amqp_url = "amqp://{}:{}@{}:{}?connection_attempts=10&retry_delay=10".format(test_rabbitmq_config["user"],
+                                                                                 test_rabbitmq_config["password"],
+                                                                                 test_rabbitmq_config["host"],
+                                                                                 test_rabbitmq_config["port"])
+    url_params = pika.URLParameters(amqp_url)
+    connection = pika.BlockingConnection(url_params)
+    chan = connection.channel()
+
+    chan.queue_declare(queue=test_rabbitmq_config["queue_name"], durable=True)
+    chan.basic_qos(prefetch_count=1)
+    chan.basic_consume(queue=test_rabbitmq_config["queue_name"],
+                       on_message_callback=receive_msg_wrapper(msg_received, chan))
+
+    chan.start_consuming()
